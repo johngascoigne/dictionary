63a722598511dfe6b5feb574c9b4e64ec7f5cee1
john gascoigne <18184@wc.school.nz>
2022-04-07 14:14:05 +1200

    Sign up and log in

--- app.py
+++ app.py
@@ -1,28 +1,104 @@
-from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
+
+from flask import Flask, render_template, request, redirect, session
 
 DB_NAME = "dictionary.db"
 
 app = Flask(__name__)
+app.secret_key = "banana"
+
+
 
 def create_connection(db_file):
     try:
         connection = sqlite3.connect(db_file)
-        connection.execute('pragma foreign_keys=ON')
         return connection
     except Error as e:
         print(e)
 
     return None
 
+
 @app.route('/')
-def render_home():
+def render_homepage():
     return render_template('home.html')
 
 @app.route('/contact')
-def render_contact():
+def render_contact_page():
     return render_template('contact.html')
 
-app.run(host='0.0.0.0', debug=True)
 
+@app.route('/login', methods=['GET', 'POST'])
+def render_login_page():
+    if is_logged_in():
+        return redirect('/')
+
+    if request.method == "POST":
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
+
+        query = """SELECT id, fname, password FROM user WHERE email = ?"""
+        con = create_connection(DB_NAME)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchall()
+        con.close()
+
+        try:
+            userid = user_data[0][0]
+            firstname = user_data[0][1]
+            db_password = user_data[0][2]
+        except IndexError:
+            return redirect('/login?error=Email+or+password+incorrect')
+
+        if db_password != password:
+            return redirect('/login?error=Email+or+password+incorrect')
+
+        session['email'] = email
+        session['userid'] = userid
+        session['firstname'] = firstname
+        print(session)
+        return redirect('/')
+    return render_template('login.html', logged_in=is_logged_in())
+
+@app.route('/signup', methods=['GET', 'POST'])
+def render_signup_page():
+    if request.method == "POST":
+        print(request.form)
+        fname = request.form.get('fname').strip().title()
+        lname = request.form.get('lname').strip().title()
+        email = request.form.get('email').strip().lower()
+        password = request.form.get('password').strip()
+        password2 = request.form.get('password2').strip()
+
+        if password != password2:
+            return redirect('/signup?error=Passwords+dont+match')
+
+        if len(password) < 8:
+            return redirect('/signup?error=Password+must+be+at+least+8+characters')
+
+        con = create_connection(DB_NAME)
+
+        query = "INSERT INTO user(id, fname, lname, email, password) VALUES(NULL,?,?,?,?)"
+
+        cur = con.cursor()
+        try:
+            cur.execute(query, (fname, lname, email, password))
+        except sqlite3.IntegrityError:
+            return  redirect('/signup?error=Email+is+already+taken')
+        con.commit()
+        con.close()
+        return redirect('/login')
+
+    return render_template('signup.html')
+
+def is_logged_in():
+    if session.get("email") is None:
+        print("Not logged in")
+        return False
+    else:
+        print("Logged in")
+        return True
+
+app.run(host='0.0.0.0', debug=True)
